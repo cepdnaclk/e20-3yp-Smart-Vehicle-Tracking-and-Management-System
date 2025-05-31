4959f8434d31ae8c3073486f2c2ff75357df6fb4
U.P.S.D.DULAJ <e20456@eng.pdn.ac.lk>
2025-06-01 03:11:47 +0530

    Vite configuration

--- code/admin-frontend/vite.config.js
+++ code/admin-frontend/vite.config.js
@@ -4,10 +4,23 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Set the base URL for assets
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          chart: ['chart.js', 'chartjs-plugin-datalabels'],
+          leaflet: ['leaflet', 'react-leaflet'],
+        },
+      },
+    },
+  },
   server: {
     proxy: {
-      "/api":
-        "https://trackmasterpro-faethrezd6cvauee.southindia-01.azurewebsites.net/", // Proxy API requests to backend
+      "/api": "https://trackmasterpro-faethrezd6cvauee.southindia-01.azurewebsites.net/", // Proxy API requests to backend
     },
   },
 });
